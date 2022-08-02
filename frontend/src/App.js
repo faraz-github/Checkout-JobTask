5686194d41f4fb76490ac436811cbd2c9d90b183
Faraz Ahmad <farazahmad.email@gmail.com>
2022-08-02 10:32:30 +0530

    Add MUI and theme context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
+import { ThemeProvider, Typography } from "@mui/material";
+
+import theme from "./MUI/theme";
+
 function App() {
-  return (
-    <div>
-      Hello World!
-    </div>
-  );
+  return <ThemeProvider theme={theme}>
+    <Typography variant="h1">Hello World!</Typography>
+  </ThemeProvider>
 }
 
 export default App;
